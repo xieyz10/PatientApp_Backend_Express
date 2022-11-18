ca524470dee8a424ea5579a0713ad1d609045061
xieyz10 <54917731+xieyz10@users.noreply.github.com>
2022-11-18 18:44:23 -0500

    fix issues

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ var ipaddress = process.env.IP;
 
 var uristring =
   process.env.MONGODB_URI ||
-  // 'mongodb://127.0.0.1:27017/patientCareApp';
-  'mongodb+srv://MAPD712PatientApp:AYEZGNZeFw9cclQk@cluster0.uzxamyj.mongodb.net/?retryWrites=true&w=majority'
+  'mongodb://127.0.0.1:27017/patientCareApp';
+// 'mongodb+srv://MAPD712PatientApp:AYEZGNZeFw9cclQk@cluster0.uzxamyj.mongodb.net/?retryWrites=true&w=majority'
 
 
 mongoose.connect(uristring, { useNewUrlParser: true });
@@ -50,18 +50,18 @@ const userSchema = new mongoose.Schema({
 })
 
 const patientSchema = new mongoose.Schema({
-  patientUserName:String,
+  patientUserName: String,
   firstName: String,
-  lastName:String,
-  address:String,
+  lastName: String,
+  address: String,
   dateOfBirth: String,
   doctorName: String,
   doctorID: String,
   sex: String,
-  phoneNumber:String,
-  emailAddress:String,
-  emergencyContact:String,
-  emergencyContactPhoneNumber:String,
+  phoneNumber: String,
+  emailAddress: String,
+  emergencyContact: String,
+  emergencyContactPhoneNumber: String,
   bedNumber: String,
   imageUri: String,
   imageType: String,
@@ -69,13 +69,13 @@ const patientSchema = new mongoose.Schema({
 })
 
 const appointmentSchema = new mongoose.Schema({
-  patientName:String,
-  address:String,
+  patientName: String,
+  address: String,
   doctorID: String,
-  phoneNumber:String,
-  emailAddress:String,
-  appointmentTime:String,
-  patientSymptom:String,
+  phoneNumber: String,
+  emailAddress: String,
+  appointmentTime: String,
+  patientSymptom: String,
   imageUri: String,
   imageType: String,
   imageName: String,
@@ -127,7 +127,7 @@ app.post('/register', function (req, res) {
   }
 
   //upload image
-  var des_file = "resources/userImage/" + req.body.username+'.jpg'
+  var des_file = "resources/userImage/" + req.body.username + '.jpg'
   fs.readFile(url.fileURLToPath(req.body.imageUri), function (err, data) {
     fs.writeFile(des_file, data, function (err) {
       if (err) {
@@ -146,12 +146,12 @@ app.post('/register', function (req, res) {
     dateOfBirth: req.body.dateOfBirth,
     emailAddress: req.body.emailAddress,
     phoneNumber: req.body.phoneNumber,
-    imageUri: url.pathToFileURL(__dirname+"/resources/userImage/"+ req.body.username)+'.jpg',
+    imageUri: url.pathToFileURL(__dirname + "/resources/userImage/" + req.body.username) + '.jpg',
     imageType: req.body.imageType,
     imageName: req.body.imageName
   });
 
-  console.log("newUser.imageuri is"+newUser.imageUri)
+  console.log("newUser.imageuri is" + newUser.imageUri)
 
   // Create the new user and saving to db
   newUser.save(function (error, result) {
@@ -174,24 +174,40 @@ app.post('/createPatient', function (req, res) {
     throw new Error("username cannot be empty")
   }
 
+  //upload image
+  var des_file = "resources/patientImage/" + req.body.patientName + '.jpg'
+  fs.readFile(url.fileURLToPath(req.body.imageUri), function (err, data) {
+    fs.writeFile(des_file, data, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Success!");
+      }
+    });
+  });
+
   // Creating new user.
   var newPatient = new Patient({
-    patientUserName:req.body.patientUserName,
+    patientUserName: req.body.patientUserName,
     firstName: req.body.firstName,
-    lastName:req.body.lastName,
-    address:req.body.address,
+    lastName: req.body.lastName,
+    address: req.body.address,
     dateOfBirth: req.body.dateOfBirth,
     doctorName: req.body.doctorName,
     doctorID: req.body.doctorID,
     sex: req.body.sex,
-    phoneNumber:req.body.phoneNumber,
-    emailAddress:req.body.emailAddress,
-    emergencyContact:req.body.emergencyContact,
-    emergencyContactPhoneNumber:req.body.emergencyContactPhoneNumber,
+    phoneNumber: req.body.phoneNumber,
+    emailAddress: req.body.emailAddress,
+    emergencyContact: req.body.emergencyContact,
+    emergencyContactPhoneNumber: req.body.emergencyContactPhoneNumber,
     bedNumber: req.body.bedNumber,
+    imageUri: url.pathToFileURL(__dirname + "/resources/patientImage/" + req.body.patientUserName) + '.jpg',
     imageType: req.body.imageType,
     imageName: req.body.imageName,
   });
+
+
+
   // Create the new user and saving to db
   newPatient.save(function (error, result) {
     // If there are any errors, pass them to next in the correct format
@@ -204,9 +220,9 @@ app.post('/createPatient', function (req, res) {
 //User Login
 app.get('/login', function (req, res, next) {
   var collection = db.collection('users');
-  collection.findOne({username: req.query.username, password:req.query.password }, function (err, user) {
+  collection.findOne({ username: req.query.username, password: req.query.password }, function (err, user) {
     if (err) throw err;
-    if (user){
+    if (user) {
       console.log(user)
       // res.send(user)
       res.json(200, user)
@@ -219,10 +235,10 @@ app.get('/login', function (req, res, next) {
 //find all patients by doctor ID
 app.get('/patients', function (req, res, next) {
   var collection = db.collection('patients');
-  collection.find({doctorID: req.query.doctorID}).toArray(function (err, patients) {
-    if(patients){
+  collection.find({ doctorID: req.query.doctorID }).toArray(function (err, patients) {
+    if (patients) {
       res.json(200, patients)
-    }else{
+    } else {
       res.send(404)
     }
   });
@@ -241,7 +257,7 @@ app.post('/createAppointment', function (req, res) {
     throw new Error("patientName cannot be empty")
   }
   //upload image
-  var des_file = "resources/appointmentAvatar/" + req.body.patientName+'.jpg'
+  var des_file = "resources/appointmentAvatar/" + req.body.patientName + '.jpg'
   fs.readFile(url.fileURLToPath(req.body.imageUri), function (err, data) {
     fs.writeFile(des_file, data, function (err) {
       if (err) {
@@ -254,14 +270,14 @@ app.post('/createAppointment', function (req, res) {
 
   // Creating new user.
   var newAppointment = new Appointment({
-    patientName:req.body.patientName,
+    patientName: req.body.patientName,
     address: req.body.address,
-    doctorID:req.body.doctorID,
-    phoneNumber:req.body.phoneNumber,
+    doctorID: req.body.doctorID,
+    phoneNumber: req.body.phoneNumber,
     emailAddress: req.body.emailAddress,
     appointmentTime: req.body.appointmentTime,
     patientSymptom: req.body.patientSymptom,
-    imageUri: url.pathToFileURL(__dirname+"/resources/appointmentAvatar/"+ req.body.patientName)+'.jpg'
+    imageUri: url.pathToFileURL(__dirname + "/resources/appointmentAvatar/" + req.body.patientName) + '.jpg'
   });
 
   // Create the new user and saving to db
@@ -276,10 +292,10 @@ app.post('/createAppointment', function (req, res) {
 //find all appointments by doctor ID
 app.get('/appointments', function (req, res, next) {
   var collection = db.collection('appointments');
-  collection.find({doctorID: req.query.doctorID}).toArray(function (err, appointments) {
-    if(appointments){
+  collection.find({ doctorID: req.query.doctorID }).toArray(function (err, appointments) {
+    if (appointments) {
       res.json(200, appointments)
-    }else{
+    } else {
       res.send(404)
     }
   });
